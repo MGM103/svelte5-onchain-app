38b49a2b3f3ffd97b696805b20b8d3efb325c6e2
MGM103 <marcus.marinelli3@gmail.com>
2024-11-06 15:37:51 +1100

    Adding infinite scroll

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,12 +6,57 @@
 
 	// STATE VARS
 	let landingPageVisible = false;
+	let fundRaisingPromoVisible = false;
+	let donationPromoVisible = false;
+	let fundRaisingContainer;
+	let donationContainer;
+
+	const observerOptions = {
+		root: null,
+		rootMargin: '0px',
+		threshold: [0, 0.2, 0.4, 0.6, 0.8, 1]
+	};
 
 	// LIFECYCLE LOGIC
 	onMount(() => {
 		landingPageVisible = true;
 		window.scrollTo(0, 0);
+
+		const fundRaisingPromoObserver = new IntersectionObserver(
+			renderFundRaisingPromo,
+			observerOptions
+		);
+		const donationPromoObserver = new IntersectionObserver(renderDonationPromo, observerOptions);
+
+		fundRaisingPromoObserver.observe(fundRaisingContainer);
+		donationPromoObserver.observe(donationContainer);
+
+		return () => {
+			fundRaisingPromoObserver.unobserve(fundRaisingContainer);
+			donationPromoObserver.unobserve(donationContainer);
+		};
 	});
+
+	// UTILITY FUNCTIONS
+	const renderFundRaisingPromo = (entries) => {
+		const [entry] = entries;
+
+		if (entry.intersectionRatio >= 0.6) {
+			fundRaisingPromoVisible = true;
+		} else {
+			fundRaisingPromoVisible = false;
+		}
+	};
+
+	const renderDonationPromo = (entries) => {
+		const [entry] = entries;
+
+		if (entry.intersectionRatio >= 0.6) {
+			donationPromoVisible = true;
+		} else {
+			donationPromoVisible = false;
+		}
+	};
 </script>
 
 <div id="landing-container">
@@ -25,9 +70,12 @@
 			<p in:typewriter={{ delay: 2200 }}>Harness the power of crowd-sourced fundraising today</p>
 		{/if}
 	</section>
-	<section class="promo-section">
-		<div class="promo-container">
-			<div class="promo-card">
+	<section
+		bind:this={fundRaisingContainer}
+		class={`promo-section ${fundRaisingPromoVisible ? 'fade-in' : 'fade-out'}`}
+	>
+		<div class="promo-container" transition:fade>
+			<div class="promo-card card">
 				<h2>Raise funds for a special cause</h2>
 				<p>
 					This platform allows you to leverage the power of smart contracts on the blockchain to
@@ -43,20 +91,23 @@
 				<button class="shadow-btn" on:click={() => goto('/fund-yourself')}>Fund yourself</button>
 			</div>
 			<div class="promo-icon-container">
-				<div class="promo-icon-card">
+				<div class="promo-icon-card card">
 					<GlobeSVG />
 					<p>Raise globally</p>
 				</div>
-				<div class="promo-icon-card">
+				<div class="promo-icon-card card">
 					<ChainsSVG />
 					<p>Raise without censorship</p>
 				</div>
 			</div>
 		</div>
 	</section>
-	<section class="promo-section">
+	<section
+		class={`promo-section ${donationPromoVisible ? 'fade-in' : 'fade-out'}`}
+		bind:this={donationContainer}
+	>
 		<div class="promo-container">
-			<div class="promo-card">
+			<div class="promo-card card">
 				<h2>Donate to initiatives around the world</h2>
 				<p>
 					Donate to unique initiatives around the globe and aid different communities and people to
@@ -71,11 +122,11 @@
 				<button class="shadow-btn" on:click={() => goto('/fund-someone')}>Fund someone</button>
 			</div>
 			<div class="promo-icon-container">
-				<div class="promo-icon-card">
+				<div class="promo-icon-card card">
 					<AidSVG />
 					<p>Aid communities across the world</p>
 				</div>
-				<div class="promo-icon-card">
+				<div class="promo-icon-card card">
 					<AnonymousSVG />
 					<p>Pseudo-anonymous donations by default</p>
 				</div>
@@ -111,9 +162,20 @@
 	}
 
 	.promo-section {
-		min-height: 50vh;
-		padding-block: 4rem;
+		min-height: max-content;
+		opacity: 0;
+		padding-block: 5rem;
 		position: relative;
+	}
+
+	.card {
+		background-color: var(--surface-2);
+		border-radius: 20px;
+		border-top: 4px solid var(--accent);
+		box-shadow:
+			0 2px 2px var(--shadow-1),
+			0 4px 4px var(--shadow-2);
+		padding: 2rem;
 	}
 
 	.promo-container {
@@ -121,14 +183,7 @@
 		gap: 2.5rem;
 
 		.promo-card {
-			background-color: var(--surface-2);
-			border-radius: 20px;
-			border-top: 4px solid var(--accent);
-			box-shadow:
-				0 2px 2px var(--shadow-1),
-				0 4px 4px var(--shadow-2);
 			flex: 1;
-			padding: 2rem;
 
 			button {
 				margin: 1rem 0;
@@ -142,12 +197,6 @@
 
 			.promo-icon-card {
 				align-items: center;
-				background-color: var(--surface-2);
-				border-radius: 20px;
-				border-top: 4px solid var(--accent);
-				box-shadow:
-					0 2px 2px var(--shadow-1),
-					0 4px 4px var(--shadow-2);
 				display: flex;
 				flex-direction: column;
 				flex: 1;
